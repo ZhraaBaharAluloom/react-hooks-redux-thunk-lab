928173e39c46229e37530b59642650c90e119720
Zahraa <alwarda@hotmail.co.uk>
2021-09-09 13:05:12 -0700

    create store

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { createStore } from "redux";
+import catsReducer from "./features/cats/catsSlice";
+import { Provider } from "react-redux";
+import { applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
+
+const store = createStore(catsReducer, composedEnhancer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    ,
+    <App />,
+  </Provider>,
+  document.getElementById("root")
+);
